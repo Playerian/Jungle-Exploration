383779365726b14759e8066ddd5a15aef059a304
Playerian <aa583045912@gmail.com>
2017-12-17 18:09:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -297,14 +297,14 @@ $(".B").click(function(){
     if ( (X !== 0 || Y !== 0) && Step !== 10 && !(BeastFind === false && X >= 100) && !(BeastX === X && BeastY === Y) &&  
        !((X > 20 || X < -20) && (Y > 20 || Y < -20) && Plane === false) && !(BeastStep === false && X >= 35) && !(TombA === false && X === -27 && Y <= 10) &&
        !(TombB === false && X >= 10 && Y === 36) && !(TombC === false && X === -27 && Y === 36) && !(Food > 100 && TomatoSatis === false) && 
-       !(InFog === false && Y >= 75) && !(InFog === true)  
+       !(InFog === false && Y <= -75) && !(InFog === true)  
        ){
     Events("Nothing","Nothing","Nothing","Shrub","Shrub","Herb","Herb","Wilding","Wilding","Monkey","Monkey","Sharpen","Sharpen","Compass");
         //Total of 14
         return;
     }
     //Fog Events
-    if (InFog === true && Y >= 75){
+    if (InFog === true && Y <= -75){
     }
     
     //Back to the square
@@ -352,13 +352,13 @@ $(".B").click(function(){
     }
     
     //Go into the Fog!
-    if (InFog === false && Y >= 75){
+    if (InFog === false && Y <= -75){
         Say("You come into a foggy area, you can't even see the road ahead or the leaves above you.");
         InFog = true;
     }
     
     //I'm Out!
-    if (InFog === true && Y < 75){
+    if (InFog === true && Y > -75){
         Say("You are out from the foggy area, you look behind, you hope you never go in there again.");
         InFog = false;
     }
