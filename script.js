b5626338b72e0f032d3b900489ff7b09770a1ef6
Playerian <aa583045912@gmail.com>
2017-12-20 21:23:28 -0800

    Update script.js

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ var InDeep = false;
 var SnakeKilled = 0;
 var MonkeyKilled = 0;
 
-$("#b8").hide
-$("#b9").hide
+$("#b8").hide();
+$("#b9").hide();
 
 //Function declare area
     //Shortcuts
@@ -650,23 +650,23 @@ function EventList(){
     //Black Village
     if( X === 50 && Y === -30 ){
     Say ("You have encountered the Black Village, a place where the shadows of the past take form.....    You trade with them and can get weapons in exchange for food."); 
-            $("#b8").show
-            $("#b9").show  
+            $("#b8").show();
+            $("#b9").show();
             $("#b1" + "#b2" + "#b3" + "#b4").hide();
         return;
     }
     
     $("#b8").click(function(){
-    Food - 3
-    WD + 5
-    $("#b8").hide()
-    $("#b9").hide()
+    Food -= 3;
+    WD += 5;
+    $("#b8").hide();
+    $("#b9").hide();
     $("#b1" + "#b2" + "#b3" + "#b4").show();
 }); 
 
 $("#b9").click(function(){
-    $("#b8").hide()
-    $("#b9").hide()
+    $("#b8").hide();
+    $("#b9").hide();
     $("#b1" + "#b2" + "#b3" + "#b4").show();
 }); 
 
